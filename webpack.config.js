9a0d6817561d4d391440bc7c967dbce09db17349
Suresh Vyas <vyassuresh808@gmail.com>
2021-06-15 16:12:01 +0530

    Completed nav menu, home and services section

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,53 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/js/index.js",
   output: {
     filename: "./js/main.js",
     path: path.resolve(__dirname, "docs"),
-    assetModuleFilename: "images/[hash][ext][query]",
+    assetModuleFilename: "./images/[hash][ext][query]",
   },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "./css/[name].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/images", to: "images" }],
+    }),
+  ],
 
   module: {
     rules: [
       {
-        test: /.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource",
+        test: /\.(jpg|png|gif|woff|eot|ttf|svg)/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 5000000,
+            name: "images/[name].[ext]",
+          },
+        },
       },
       {
-        test: /\.(s[ac]|c)ss$/i,
+        test: /\.scss$/i,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
+            // options: { publicPath: "" },
           },
           {
             loader: "css-loader",
             options: {
-              importLoaders: 2,
               sourceMap: true,
             },
           },
@@ -34,7 +56,14 @@ module.exports = {
             loader: "postcss-loader",
             options: {
               postcssOptions: {
-                plugins: [require("autoprefixer")],
+                plugins: [
+                  require("autoprefixer"),
+                  require("postcss-assets")({
+                    loadPaths: ["images/"],
+                    basePath: "docs",
+                  }),
+                  require("postcss-uncss"),
+                ],
               },
               sourceMap: true,
             },
@@ -58,18 +87,5 @@ module.exports = {
     watchContentBase: true,
   },
 
-  plugins: [
-    // new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "./css/[name].css",
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-      inject: "body",
-    }),
-  ],
-
-  mode: "development",
   devtool: "source-map",
 };
